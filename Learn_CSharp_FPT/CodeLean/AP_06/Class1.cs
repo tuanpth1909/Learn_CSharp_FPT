6f640224fa0a6679bf7e8404857fafac5de0d924
tuanpth1909 <60849085+tuanpth1909@users.noreply.github.com>
2020-06-06 00:16:25 +0700

    lab 3
    push

--- Learn_CSharp_FPT/CodeLean/AP_06/Class1.cs
+++ Learn_CSharp_FPT/CodeLean/AP_06/Class1.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Learn_CSharp_FPT.CodeLean.AP_06
 {
-    class Class1
+    public static class Class1
     {
+        public static void Run()
+        {
+            ex1();
+        }
+        static void ex1()
+        {
+         public string Make { get; set; }
+        public string Model { get; set; }
+        public void MakeCappuccino()
+        {
+            //Method logic goes here
+        }
+        public void MakeEspresso()
+        {
+            //Method logic goes here
+        }
+        public event OutOfBeansHandler OutOfBeans;
+          }
+       }
     }
 }
